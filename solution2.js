fbcfee7dd311c50e030f97b01079655b0c7aa116
Michael Crump <mike@Michaels-MBP.lan>
2024-11-03 05:36:47 -0800

    first commit

---
+++ solution2.js
@@ -0,0 +1,32 @@
+//Get all the published frontend and backend courses,
+//sort them by their price and descending order,
+//pick only their name and author, and display them
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/mongo-exercises');
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String, 
+  tags: [ String ],
+  date: Date, 
+  isPublished: Boolean,
+  price: Number
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+async function getCourses() {
+  return await Course
+  .find({ isPublished: true })
+  .or([ { tags: 'frontend' }, { tags: 'backend' } ])
+  .sort('-price')
+  .select('name author price');
+}
+
+async function run() {
+  const courses = await getCourses();
+  console.log(courses);
+}
+
+run();
